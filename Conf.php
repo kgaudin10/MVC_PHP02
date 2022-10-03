4d68731594d27ac2455292e5990ea01787161916
kgaudin10 <gaudinkillian@gmail.com>
2022-10-03 16:43:30 +0200

    Ex3

---
+++ Conf.php
@@ -0,0 +1,29 @@
+<?php
+class Conf {
+static private $databases = array(
+// ou localhost sur votre machine
+'hostname' => 'localhost',
+// A l'IUT, vous avez une BDD nommee comme votre login 
+// Sur votre machine, vous devrez creer une BDD 
+'database' => 'MVC2',
+// A l'IUT, c'est votre login
+// Sur votre machine, vous avez surement un compte 'root'
+'login' => 'root',
+// A l'IUT, c'est votre mdp (INE par defaut)
+// Sur votre machine personelle, vous avez creez ce mdp a l'installation 
+'password' => 'a_remplir'
+);
+static public function getLogin() {
+//en PHP l'indice d'un tableau n'est pas forcement un chiffre. 
+return self::$databases['login'];
+}
+static public function getHostname() {
+    //en PHP l'indice d'un tableau n'est pas forcement un chiffre. 
+    return self::$databases['hostname'];
+    }
+static public function getDatabse() {
+        //en PHP l'indice d'un tableau n'est pas forcement un chiffre.
+        return self::$databases['database'];
+        }
+}
+?>
